f02b96991326dc1d0da068d962a0fe1e07de0f6d
mangopeian <mangopeian@gmail.com>
2017-12-16 01:30:30 -0800

    commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,77 @@
+var feelings = ['Excited', 'Sad', 'Angry', 'Surprised','Love'];
+
+
+	function displayGif(){
+
+		var gif = $(this).attr('data-name');
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=LbTCNbcyAZaC8oO6biR4L3nYg3IJh1rZ";
+
+		 $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+		 	console.log(response);
+			$("#feelingsView").empty();
+            for (var i = 0; i < response.data.length; i++){
+
+            	var rating = response.data[i].rating;
+                var imageUrl = response.data[i].images.fixed_height.url;
+             	var imageStillUrl = response.data[i].images.fixed_height_still.url;
+
+                var image = $("<img>");
+                var ratingText = $("<p id='rating'>" + "Rating: " + rating + "</p>");
+
+                
+                image.attr('src', imageStillUrl);
+                image.attr('alt', 'gif');
+                image.attr('data-state', 'still');
+                image.attr('data-still', imageStillUrl);
+                image.attr('data-animate', imageUrl);
+
+
+                $('#feelingsView').prepend(image, ratingText);
+                checkState ();
+            }
+		 }); 
+	} 
+
+	function renderButtons(){ 
+
+		$('#buttonsView').empty();
+
+		for (var i = 0; i < feelings.length; i++){
+
+		    var newButton = $('<button class="btn btn-primary">') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+		    newButton.addClass('feeling'); // Added a class 
+		    newButton.attr('data-name', feelings[i]); // Added a data-attribute
+		    newButton.text(feelings[i]); // Provided the initial button text
+		    $('#buttonsView').append(newButton); // Added the button to the HTML
+		}
+	}
+
+	$('#addFeeling').on('click', function(){
+
+		var feeling = $('#feeling-input').val().trim();
+
+		feelings.push(feeling);
+		
+		renderButtons();
+
+		return false;
+	})
+
+
+	$(document).on('click', '.feeling', displayGif);
+
+	renderButtons();
+
+	function checkState(){
+		$('img').on('click', function(){
+	  var state = $(this).attr('data-state'); 
+	  if (state == 'still'){
+	  $(this).attr('src', $(this).data('animate'));
+	  $(this).attr('data-state', 'animate');
+	  }else{
+	  $(this).attr('src', $(this).data('still'));
+	  $(this).attr('data-state', 'still');
+	}
+
+		});
+	};
